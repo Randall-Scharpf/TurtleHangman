ba1a0c3976883d4db1b85f3956dd0dc8eb818ec9
Randall Scharpf <randall.scharpf@gmail.com>
2021-01-13 15:40:11 -0800

    implemented console settings input

--- console.py
+++ console.py
@@ -28,16 +28,45 @@ def you_win():
 
 # asks if two or one players will be playing, and returns the boolean answer
 def ask_if_multiplayer():
-    print ("TODO")
+    while True:
+        line = input("Enter the number of players (1 or 2):")
+        if (line.strip() == "1"):
+            return False
+        if (line.strip() == "2"):
+            return True
+        print("You must enter a valid response of \"1\" or \"2\" players.")
 
 # requests the integer number of letters that is the length of the random word and returns it or zero (if length is to be unspecified)
 def ask_number_letters():
-    print ("TODO")
+    while True:
+        line = input("Enter the length of the word to guess, or \"any\" if you would like it to be randomized.")
+        if (line.strip() == "any"):
+            return 0
+        try:
+            value = int(line)
+            if (value > 3):
+                return value
+            else:
+                print("A word with which hangman is to be played must be at least 4 letters long.")
+        except ValueError:
+            print("Uh oh, you didn't enter an integer or the word \"any\"! Try again.")
 
 # asks if the user wants to play again or if they would like to exit and returns the boolean answer
 def play_again():
-    print ("TODO")
+    while True:
+        line = input("Would you like to play again? (yes/no)")
+        if (line.strip() == "yes"):
+            return True
+        if (line.strip() == "no"):
+            return False
+        print("You must answer with either \"yes\" or \"no\".")
 
 # asks if the player would like to use the same settings as the last game and returns the boolean answer
 def change_settings():
-    print ("TODO")
+    while True:
+        line = input("Would you like to change the game settings? (yes/no)")
+        if (line.strip() == "yes"):
+            return True
+        if (line.strip() == "no"):
+            return False
+        print("You must answer with either \"yes\" or \"no\".")
