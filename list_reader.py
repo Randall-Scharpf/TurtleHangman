68a96a2c11a56826aac04554348096f32ce3eb93
Aaron Choi <77123079+aarontc5@users.noreply.github.com>
2021-01-14 10:48:59 -0800

    Update list_reader.py

--- list_reader.py
+++ list_reader.py
@@ -1,3 +1,12 @@
+'''
 # quickly a random word from the English list (normalized to difficulty by default once implemented)
 def pick_random_word():
     print ("TODO")
+'''
+
+
+# picks a random word from the master word list and output it in the console.
+import random
+word = (random.choice(list(open('master word list.txt'))))
+
+print(word)
