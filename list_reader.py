4ca958a1771346020ac04cd9297bdd43fa0220ae
Randall Scharpf <randall.scharpf@gmail.com>
2021-01-14 11:04:16 -0800

    abstracted to be compatible

--- list_reader.py
+++ list_reader.py
@@ -1,3 +1,6 @@
+from random import random
+
 # quickly a random word from the English list (normalized to difficulty by default once implemented)
 def pick_random_word():
-    print ("TODO")
+    listt = list(open('master word list.txt'))
+    return listt[int(random()*len(listt))]
